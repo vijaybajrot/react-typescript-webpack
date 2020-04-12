9d27d6305b4e7c9e4463410da146c5f76cc57da7
Vijay Bajrot <vijaybajrot@gmail.com>
2020-04-12 21:01:28 +0530

    Eslint and Prettier Setup

--- app/pages/Home/index.tsx
+++ app/pages/Home/index.tsx
@@ -6,26 +6,26 @@ import { addView, connector } from "@app/utils/redux";
 import style from "./style.scss";
 
 function reducer(state = {}, action) {
-  switch (action.type) {
-    case "INIT_VIEW":
-      return { Page: "Home" };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case "INIT_VIEW":
+			return { Page: "Home" };
+		default:
+			return state;
+	}
 }
 
 addView("home", reducer);
 
 class HomePage extends React.PureComponent<any> {
-  static fetchData({ store }) {
-    console.log(moment().format("DD-MM-YYYY"));
-    return store.dispatch({ type: "INIT_VIEW", view: "home" });
-  }
-  render() {
-    //console.log({ page: this.props });
-    if (this.props.loading === true) return "Loading page";
-    return <div className={style.HomeHeading}>Home Page Updated</div>;
-  }
+	static fetchData({ store }) {
+		console.log(moment().format("DD-MM-YYYY"));
+		return store.dispatch({ type: "INIT_VIEW", view: "home" });
+	}
+	render() {
+		//console.log({ page: this.props });
+		if (this.props.loading === true) return "Loading page";
+		return <div className={style.HomeHeading}>Home Page Updated</div>;
+	}
 }
 
 export const Home = connector("home", HomePage);
